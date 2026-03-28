4e42eea0d3ce07fc41d4a7b0d7ddc2cd9ac9926f
Iyeth-Nalzz <nahlstecu@gmail.com>
2026-03-28 17:15:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 /* Hady Zen'in */
 /* dibuat oleh Hady with love - Copyright HadyZenin 2025 */
-const { spawn } = require('child_process');
-const requiredVersion = '14.x';
 
-if (!process.version.match(new RegExp(`^v${requiredVersion.replace('x', '\\d+')}$`))) {
-  console.error(`Error: Node.js versi ${requiredVersion} diperlukan.`);
-  process.exit(1);
-}
+const { spawn } = require('child_process');
 
 function hady() {
   const child = spawn("node Ara.js", {
     cwd: __dirname,
     stdio: "inherit",
     shell: true
-  });
+});
+
   child.on("close", (code) => {
     if (code == 2) {
-      hady();
-    }
-  });
+      hady(); 
+  }
+ });
 };
 hady();
